a1984311fdcd58d15a585ab05710ac4fd855c21a
Sara Gisondi <sara.gisondi@outlook.it>
2023-06-07 19:27:41 +0200

    add condition active navbar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,10 @@
 <header>
   <ul class="nav">
     <li class="nav-item active">
-      <a class="nav-link active" aria-current="page" href="{{route('home')}}">Home</a>
+      <a class="nav-link {{Route::currentRouteName() === 'home' ? 'active' : '' }} " aria-current="page" href="{{route('home')}}">Home</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="{{route('movies')}}">Film</a>
+      <a class="nav-link {{Route::currentRouteName() === 'movies' ? 'active' : '' }}" href="{{route('movies')}}">Film</a>
     </li>
   </ul>
 </header>
